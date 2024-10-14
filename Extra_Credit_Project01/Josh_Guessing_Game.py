c40d54bbd98b3e552afa0ad7ad95e0a5df416d01
myo thida <drmyothida@gmail.com>
2024-10-14 14:48:20 -0400

    Added Error Scenarios for Josh_Guessing_Game

--- Extra_Credit_Project01/Josh_Guessing_Game.py
+++ Extra_Credit_Project01/Josh_Guessing_Game.py
@@ -2,9 +2,13 @@
 # (2.5 points out of 5 points)
 # (1 point): The game successfully generates a random number and allows the player to guess it, providing feedback if the guess is too high or too low.
 # (1 point): The code checks if the guess is an integer between 0 and 1000.
-# (0.5 point): The player is prompted to decide whether to play again after winning or losing. But, the code does not end correctly if you enter 0.
+# (0.5 point): The player is prompted to decide whether to play again after winning or losing. But, the code does not end correctly if you enter 0. 
+# (comment) - Your code does not work if I won the game with less than 10 guesses and want to stop playing. It will keep asking me to play again.
+# Check Josh.png
 # (0 point): The game did not end correctly after running the 2nd round.
 # (0 point): The comments are not added to the code.
+
+
 
 condition = True
 while condition == True:
@@ -12,6 +16,7 @@ while condition == True:
     print("The number will be between 1 and 1000 ")
     import random
     number = random.randint(1, 1000)
+    print(number)
     count = 0
     while count< 10 :
         guess = (input("guess the number: "))
